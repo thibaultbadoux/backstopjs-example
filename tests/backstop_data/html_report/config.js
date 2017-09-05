be07d3e48ba382ba59cafd55af87230446c2768f
Thibault Badoux <thibault.badoux@pwc-digital.ch>
2017-09-05 11:18:57 +0200

    initial commit

---
+++ tests/backstop_data/html_report/config.js
@@ -0,0 +1,47 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/test_Home_0_c-gallery_0_mobile.png",
+        "test": "../bitmaps_test/20170828-114708/test_Home_0_c-gallery_0_mobile.png",
+        "selector": ".c-gallery",
+        "fileName": "test_Home_0_c-gallery_0_mobile.png",
+        "label": "Home",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/test_Home_0_c-gallery_1_desktop.png",
+        "test": "../bitmaps_test/20170828-114708/test_Home_0_c-gallery_1_desktop.png",
+        "selector": ".c-gallery",
+        "fileName": "test_Home_0_c-gallery_1_desktop.png",
+        "label": "Home",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    }
+  ]
+});
